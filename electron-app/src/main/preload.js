aa2ae967221fbcfb6f9f90b40b708091541f0e48
Henok <150522802+HenokAnil@users.noreply.github.com>
2026-09-07 21:56:56 +0530

    Delete electron-app directory

--- electron-app/src/main/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  openFile: () => ipcRenderer.invoke('dialog:openFile'),
-  uploadFile: (filePath) => ipcRenderer.invoke('api:uploadFile', filePath),
-  openPath: (pathStr) => ipcRenderer.invoke('app:openPath', pathStr)
-});
